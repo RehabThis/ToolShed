f8f8ecce468836ab523e1d2f148db3545bef5d28
Cody Landry <landry.cody@gmail.com>
2022-02-28 01:10:21 -0600

    Adding components

---
+++ src/components/ToolFQDN.js
@@ -0,0 +1,93 @@
+import React, {useState} from 'react'
+import { Button, Modal } from 'react-bootstrap'
+//import ModalNew from './ModalNew'
+
+function ToolFQDN() {
+    const [showModal, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const [fqdnList, setFqdnList] = useState("")
+    var fqdnMax = [];
+    const handleChange = e => {
+      setFqdnList(e.target.value)
+      fqdnMax.push = e.target.value;
+    }
+    const handleUpdate = e => {
+      if (fqdnMax != fqdnList) {
+        fqdnMax = fqdnList;
+      }
+    }
+    const [configOutput, setConfigOutput] = useState(fqdnMax.map((fqdn) => { 
+      return fqdn + " bid daddy";
+    }))
+
+
+    return (
+        <>
+        <Button variant="primary" onClick={handleShow}>
+          Launch demo modal
+        </Button>
+        <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Whitelist Config Generator</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <div className="row">
+                <div className="col-6">
+                <label>FQDN Input</label>
+                <textarea id="configInput" className="textFields" value={fqdnList} onChange={handleChange}></textarea>
+                </div>
+                <div className="col-6">
+                <label>Configuration Output</label>
+                <textarea id="configOutput" className="textFields" value={fqdnMax} onChange={handleUpdate}></textarea>
+                </div>
+            </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+        </>
+    )
+}
+
+export default ToolFQDN
+
+
+/* 
+
+function parseConfig2() {
+            var fqdnList = document.getElementById("configInput").value.split("\n");
+            var pushFQDN = ["config webfilter urlfilter\nedit 2\nset name \"Environment_URL_Filter\"\nconfig entries\n"];
+            for (i = 0; i < fqdnList.length; i++) {
+                var y = "edit 0\nset url \"" + fqdnList[i] + "\"\nset type wildcard\nset action allow\nnext\n";
+                                                                
+                                                                pushFQDN.push(y);
+                                                                }
+                                                document.getElementById("configOutput").value = pushFQDN.join("");
+        }
+
+
+
+
+*/    //    var fqdnList = document.getElementById("configInput").value.split("\n");
+//    var pushFQDN = ["config webfilter urlfilter\nedit 2\nset name \"Environment_URL_Filter\"\nconfig entries\n"];
+//    for (i = 0; i < fqdnList.length; i++) {
+//      var y = "edit 0\nset url \"" + fqdnList[i] + "\"\nset type wildcard\nset action allow\nnext\n";                                                  
+//        pushFQDN.push(y);
+//    }
+//    document.getElementById("configOutput").value = pushFQDN.join("");
+
+//var fqdnInput = configInput.value;
+
+//const [value, setValue] = useState(props.name);
+
+//const handleChange = (event) => {
+//    setValue(event.target.value);
+//};
+
