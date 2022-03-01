f168f5990bf23a929556f983fa9d4958053fc020
Landry <Cody.Landry@lumen.com>
2022-03-01 16:35:00 -0600

    push new files

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+function Navbar() {
+    return (  
+        <nav>
+            <div className="row"></div>
+                <div className="col_1">
+                    <logo>
+                        <img src="#">
+                    </logo>
+                </div>
+
+                <div className="col_2">
+                    <ul>
+                        <li>Juniper</li>
+                        <li>Cisco</li>
+                        <li>Fortigate</li>
+                        <li>Palo Alto</li>
+                    </ul>
+                </div>
+        </nav>
+    )
+
+export default Navbar
