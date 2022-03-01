f635ae38d440c323dd71ba6d98506afb860d01c5
Landry <Cody.Landry@lumen.com>
2022-03-01 13:25:49 -0600

    Set proper name for first tool and updated references

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import ToolFQDN from './components/ToolFQDN'
+import FortigateWhitelistToolFQDN from './components/FortigateWhitelistToolFQDN'
 //import ModalNew from './components/ModalNew'
 
 
 function App() {
 
-  return (
-    <div className="App">
-      <ToolFQDN />
-    </div>
-  );
+    return ( 
+      <div className = "App" >
+        <FortigateWhitelistToolFQDN / >
+        </div>
+    );
 }
 
 export default App;
