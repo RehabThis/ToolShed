f747116f6f9a7ed7abd893de87a5b035c34d6923
landryc <landry.cody@gmail.com>
2022-03-03 13:13:28 -0600

    Added lots of styling to correct button overflows. Added corrections to H1 and Subtitles styling.

--- src/components/Fortigate.js
+++ src/components/Fortigate.js
@@ -1,38 +1,63 @@
-import React from 'react'
-import {Button} from 'react-bootstrap'
-import Lumen_Technologies_Logo from '../imgs/Lumen_Technologies_Logo.png'
-import { Link } from 'react-router-dom'
+import React from "react"
+import { Button } from "react-bootstrap"
+import Lumen_Technologies_Logo from "../imgs/Lumen_Technologies_Logo.png"
+import { Link } from "react-router-dom"
 
 function Fortigate() {
-    return (  
-        <>
-        <div className="appContainer">
-            <div id="logoCol">
-                <logo className="logo-main">
-                    <h1 className="appTitle">ToolShed</h1>
-                    <p className="subTitle">Current Tool Shed: Fortigate</p>
-                    {/* <p className="subTitle">created by</p>
-                    <img src={Lumen_Technologies_Logo} className="logoImg"></img> */}
-                </logo>
-            </div>
-            <div id="navCol" className="fortiCol">
-                <ul>
-                    <Button className="navLinks" variant="info"><Link to="/vendor/fortigate/whitelist-tool/"><li>Whitelist</li></Link></Button>
-                    <Button className="navLinks" variant="info"><li>FW Rule</li></Button>
-                    <Button className="navLinks" variant="info"><li>Route Map</li></Button>
-                    <Button className="navLinks" variant="info"><li>Static Route</li></Button> 
-                </ul>
-                <ul>    
-                    <Button className="navLinks" variant="info"><li>VPN</li></Button>
-                    <Button className="navLinks" variant="info"><li>Static Route</li></Button>  
-                    <Button className="navLinks" variant="info"><li>BGP Peer</li></Button>
-                    <Button className="navLinks" variant="info"><li>VDOM</li></Button> 
-                </ul>
-            </div>
-            <div><Link to="/"><Button className="navLinks" variant="info"><li>Back</li></Button></Link></div>
+  return (
+    <>
+      <div className="appContainer">
+        <div classNam="row">
+          <div id="logoCol">
+            <logo className="logo-main">
+              <h1 className="appTitle">ToolShed</h1>
+              <p className="subTitle">Current Tool Shed: Fortigate</p>
+            </logo>
+          </div>
+          <br />
+          <div id="navCol" className="fortiCol">
+            <ul className="navUL">
+              <Button className="navLinks" variant="info">
+                <Link to="/vendor/fortigate/whitelist-tool/">
+                  <li>Whitelist</li>
+                </Link>
+              </Button>
+              <Button className="navLinks" variant="info">
+                <li>FW Rule</li>
+              </Button>
+              <Button className="navLinks" variant="info">
+                <li>Route Map</li>
+              </Button>
+              <Button className="navLinks" variant="info">
+                <li>Static Route</li>
+              </Button>
+            </ul>
+            <ul className="navUL">
+              <Button className="navLinks" variant="info">
+                <li>VPN</li>
+              </Button>
+              <Button className="navLinks" variant="info">
+                <li>IP Objects</li>
+              </Button>
+              <Button className="navLinks" variant="info">
+                <li>BGP Peer</li>
+              </Button>
+              <Button className="navLinks" variant="info">
+                <li>VDOM</li>
+              </Button>
+            </ul>
+          </div>
         </div>
-        </>
-    )
+        <div>
+          <Link to="/">
+            <Button className="navLinks" variant="info">
+              <li>Back</li>
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Fortigate
--- src/components/FortigateWhitelistToolFQDN.js
+++ src/components/FortigateWhitelistToolFQDN.js
@@ -1,81 +1,87 @@
-import React, {useState} from 'react'
-import { Button, Modal } from 'react-bootstrap'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react"
+import { Button, Modal } from "react-bootstrap"
+import { Link } from "react-router-dom"
 //import ModalNew from './ModalNew'
 
 function parseConfig2(fqdnArray) {
-  var fqdnList = fqdnArray.split("\n");
-//  var pushFQDN = ["config webfilter urlfilter\nedit 2\nset name \"Environment_URL_Filter\"\nconfig entries\n"];
-  var ret = fqdnList.map((e) => {
-    return "edit 0\nset url \"" + e + "\"\nset type wildcard\nset action allow\nnext\n";
-  }).join("");
-  return "config webfilter urlfilter\nedit 2\nset name \"Environment_URL_Filter\"\nconfig entries\n" + ret;
-  //return fqdnList.join("block");
+  var fqdnList = fqdnArray.split("\n")
+  var ret = fqdnList
+    .map((e) => {
+      return 'edit 0\nset url "' + e + '"\nset type wildcard\nset action allow\nnext\n'
+    })
+    .join("")
+  return 'config webfilter urlfilter\nedit 2\nset name "Environment_URL_Filter"\nconfig entries\n' + ret + "end"
 }
 
 function handleCopy(handleOutput) {
-  navigator.clipboard.writeText(handleOutput);
-  console.log(handleOutput);
+  navigator.clipboard.writeText(handleOutput)
+  console.log(handleOutput)
 }
 
 function FortigateWhitelistToolFQDN() {
-    var [showModal, setShow] = useState(true);
-    var handleClose = () => setShow(false);
-    var handleShow = () => setShow(true);
+  var [showModal, setShow] = useState(true)
+  var handleClose = () => setShow(false)
+  var handleShow = () => setShow(true)
 
-    var [fqdnList, setFqdnList] = useState("");
-    var [output, setOutput] = useState("");
-    var handleChange = e => {
-        setFqdnList(e.target.value);
-        setOutput(parseConfig2(fqdnList));
-    } 
+  var [fqdnList, setFqdnList] = useState("")
+  var [output, setOutput] = useState("")
+  var handleChange = (e) => {
+    setFqdnList(e.target.value)
+    setOutput(parseConfig2(fqdnList))
+  }
 
+  var handleOutput = (e) => {
+    setOutput(parseConfig2(fqdnList))
+  }
 
-    var handleOutput = e => {
-      setOutput(parseConfig2(fqdnList));
-    }
-
-    return (
-        <div className="appContainer">
-          
-          <h1 className="appTitle">FortiWhiteList Config Gen</h1>
+  return (
+    <div className="appContainer">
+      <div>
+        <h1 className="appTitle">FortiWhiteList Config Gen</h1> <br />
         <p className="subTitle">This tool will generate the configuration input required to apply a new URL to an existing Fortigate Content Filter. Input a list of FQDN's on the left text area and the right will auto-populate with the template. You must drop down a line after each FQDN including the last one.</p>
-         
-        <Button variant="info" className="navLinks" onClick={handleShow}><li>Whitelist</li></Button>
-        <Modal show={showModal} onHide={handleClose}>
+        <br />
+        <Button variant="info" className="navLinks" onClick={handleShow}>
+          <li>Whitelist</li>
+        </Button>
+      </div>
+
+      <Modal show={showModal} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Whitelist Config Generator</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <div className="row">
-                <div className="col-6">
-                <label>FQDN Input</label>
-                <textarea className="textFields" value={fqdnList} onChange={handleChange}></textarea>
-                </div>
-                <div className="col-6">
-                <label>Configuration Output</label>
-                <textarea className="textFields" value={output} onChange={handleOutput}></textarea>
-                </div>
+          <div className="row">
+            <div className="col-6">
+              <label>FQDN Input</label>
+              <textarea className="textFields" value={fqdnList} onChange={handleChange}></textarea>
             </div>
+            <div className="col-6">
+              <label>Configuration</label>
+              <textarea className="textFields" value={output} onChange={handleOutput}></textarea>
+            </div>
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="info" onClick={handleClose}>
             Close
-          </Button> 
+          </Button>
           <Button variant="info" onClick={handleCopy}>
-            Copy to Clipboard 
+            Copy to Clipboard
           </Button>
         </Modal.Footer>
       </Modal>
       <div className="backButton">
-      <Link to="/vendor/fortigate"><Button className="navLinks" variant="info"><li>Back</li></Button></Link>
+        <Link to="/vendor/fortigate">
+          <Button className="navLinks" variant="info">
+            <li>Back</li>
+          </Button>
+        </Link>
       </div>
-        </div>
-    )
+    </div>
+  )
 }
 
 export default FortigateWhitelistToolFQDN
-
 
 /* 
 
@@ -89,7 +95,7 @@ All document.getElementById's
 */ //    var fqdnList = document.getElementById("configInput").value.split("\n");
 //    var pushFQDN = ["config webfilter urlfilter\nedit 2\nset name \"Environment_URL_Filter\"\nconfig entries\n"];
 //    for (i = 0; i < fqdnList.length; i++) {
-//      var y = "edit 0\nset url \"" + fqdnList[i] + "\"\nset type wildcard\nset action allow\nnext\n";                                                  
+//      var y = "edit 0\nset url \"" + fqdnList[i] + "\"\nset type wildcard\nset action allow\nnext\n";
 //        pushFQDN.push(y);
 //    }
 //    document.getElementById("configOutput").value = pushFQDN.join("");
@@ -101,9 +107,6 @@ All document.getElementById's
 //const handleChange = (event) => {
 //    setValue(event.target.value);
 //};
-
-
-
 
 /*
 
