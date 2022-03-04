96e3b730530356649f5637e31b1d70ffc0b84f3e
Cody Landry <landry.cody@gmail.com>
2022-03-03 22:16:23 -0600

    Remove reference to previously removed image

--- src/components/Fortigate.js
+++ src/components/Fortigate.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Button } from "react-bootstrap"
-import Lumen_Technologies_Logo from "../imgs/Lumen_Technologies_Logo.png"
 import { Link } from "react-router-dom"
 
 function Fortigate() {
