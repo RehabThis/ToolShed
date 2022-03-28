152a1445f48a3b524a400fbce7d936d5f045b2a7
landryc <landry.cody@gmail.com>
2022-03-28 16:17:37 -0500

    Finished the conditional logic of the form for FortigateFWRule and created a final submit button

--- src/components/FortigateFWRule.js
+++ src/components/FortigateFWRule.js
@@ -22,6 +22,18 @@ function FortigateFWRule() {
     multiVdomQuestion: "undefined",
     fwRuleName: "",
     allowOrDeny: "",
+    srcIntfName: "",
+    dstIntfName: "",
+    newSrcObjReq: "",
+    extSrcObjList: "",
+    newSrcObjList: "",
+    newSrcObjGrpReq: "",
+    newDstObjReq: "",
+    newDestIpList: "",
+    extDestIpList: "",
+    newDstObjGrpReq: "",
+    ruleEnabled: "",
+    ruleDescription: "",
   })
 
   var handleChange = (e) => {
@@ -72,12 +84,12 @@ function FortigateFWRule() {
                       VDOM Name:<input name="vdomName" type="text" placeholder="Example: Customer-VDOM-355"></input>
                     </label>
                     </> 
-                  )}
+                  ) || (<></>)}
             {(state.multiVdomQuestion !== 'undefined' &&               
               <label name="fwRuleName">
                 Firewall Policy Name:<input name="fwRuleName" type="text" value={state.fwRuleName} onChange={handleChange} placeholder="Example: Windows-Update-Service"></input>
               </label>
-              )}
+              ) || (<></>)}
 
             {( state.fwRuleName !== '' && 
               <label name="allowOrDeny">
@@ -88,7 +100,7 @@ function FortigateFWRule() {
                   <option value="denyRule">Deny</option>
                 </select>
               </label>
-            )}
+            ) || (<></>)}
             
             {(state.allowOrDeny !== '' && 
                 <label name="portCreationReqL">
@@ -99,7 +111,7 @@ function FortigateFWRule() {
                   <option value="false">No</option>
                 </select>
               </label>
-            )}
+            ) || (<></>)}
 
               
              { (state.portCreationReq === 'false' &&                   
@@ -138,64 +150,97 @@ function FortigateFWRule() {
                     </input>
                 </label> ) || (<></>)}
                   
-               {/*
-              <label name="srcIntfName">
-                What is the Source Interface Name? (srcintf):<input name="srcIntfName" type="text" placeholder="Example: Inside_Vlan"></input>
-              </label>
-              <label name="dstIntfName">
-                What is the Destination Interface Name? (dstintf):<input name="dstIntfName" type="text" placeholder="Example: Outside_Vlan"></input>
-              </label>
-              <label name="newSrcObjReq">
-                Do the source address IP objects exist currently?
-                <select name="newSrcObjReq" onChange={handleChange}>
+              {(state.portCreationReq !== '' && 
+                  <label name="srcIntfNameL">
+                    What is the Source Interface Name? (srcintf):<input value={state.srcIntfName} onChange={handleChange} name="srcIntfName" type="text" placeholder="Example: Inside_Vlan"></input>
+                  </label>            
+              ) || (<></>)} 
+              {(state.srcIntfName !== '' && 
+                  <label name="dstIntfNameL">
+                    What is the Destination Interface Name? (dstintf):<input value={state.dstIntfName} onChange={handleChange} name="dstIntfName" type="text" placeholder="Example: Outside_Vlan"></input>
+                  </label>            
+              ) || (<></>)}            
+              {(state.dstIntfName !== '' &&
+                <label name="newSrcObjReqL">
+                  Do you require new Source IP Objects?
+                <select name="newSrcObjReq" value={state.newSrcObjReq} onChange={handleChange}>
                   <option value="undefined">-- Please Choose</option>
                   <option value="srcYes">Yes</option>
                   <option value="srcNo">No</option>
                 </select>
+                </label>            
+              ) || (<></>)}
+              {(state.newSrcObjReq === 'srcNo' && 
+                <label name="extSrcObjListL">
+                  Please provide the name of the existing Source IP Object(s), Object-Groups (one per line w/quotes around them).
+                <textarea className="textFields" value={state.extSrcObjList} onChange={handleChange} name="extSrcObjList"></textarea>
+                </label>
+                            
+              ) || (state.newSrcObjReq === 'srcYes' && 
+                <label name="newSrcObjListL">
+                  Please provide the name of the new Source IP Object(s) (one per line w/quotes around them).
+                <textarea className="textFields" value={state.newSrcObjList} onChange={handleChange} name="newSrcObjList"></textarea>
               </label>
-              <label name="newSrcObjList">
-                Please provide the name of the existing Source IP Object(s), Object-Groups (one per line w/quotes around them)?
-                <textarea className="textFields" name="newSrcObjList"></textarea>
-              </label>
-              <label name="newSrcObjGrpReq">
-                Would you like to create a new object group for the Source IPs?
-                <select name="newSrcObjGrpReq">
+              ) }
+            {(state.newSrcObjReq === 'srcYes' && state.newSrcObjList !== '' && 
+            <label name="newSrcObjGrpReqL">
+              Would you like to create a new object group for the Source IPs?
+              <select name="newSrcObjGrpReq" value={state.newSrcObjGrpReq} onChange={handleChange}>
+                <option value="undefined">-- Please Choose</option>
+                <option value="true">Yes</option>
+                <option value="false">No</option>
+              </select>
+            </label>                    
+            )}
+              {(state.newSrcObjReq !== '' && 
+              <label name="newDstObjReqL">
+                Do you require new Destination IP Objects?
+                <select name="newDstObjReq" value={state.newDstObjReq} onChange={handleChange}>
                   <option value="undefined">-- Please Choose</option>
                   <option value="true">Yes</option>
                   <option value="false">No</option>
                 </select>
-              </label>
-              <label name="newDstObjReq">
-                Do the destination address IP objects exist currently?
-                <select name="newDstObjReq">
-                  <option value="undefined">-- Please Choose</option>
-                  <option value="true">Yes</option>
-                  <option value="false">No</option>
-                </select>
-              </label>
-              <label name="newDestIpList">
+              </label>            
+              )}
+              {(state.newDstObjReq === 'false' && 
+              <label name="extDestIpListL">
                 Please provide the name of the existing Destination IP Object(s), Object-Groups (one per line w/quotes around them)?
-                <textarea className="textFields" name="newDestIpList"></textarea>
-              </label>
-              <label name="newDstObjGrpReq">
+                <textarea className="textFields" value={state.extDestIpList} onChange={handleChange} name="extDestIpList"></textarea>
+              </label>            
+              ) || (state.newDstObjReq === 'true' && 
+              <label name="newDestIpListL">
+                Please provide the name of the new Destination IP Object(s)(one per line w/quotes around them)?
+                <textarea className="textFields" value={state.newDestIpList} onChange={handleChange} name="newDestIpList"></textarea>
+              </label>                          
+              )}
+              {(state.newDestIpList !== '' && 
+              <label name="newDstObjGrpReqL">
                 Would you like to create a new object group for the Destination IPs?
-                <select name="newDstObjGrpReq">
+                <select name="newDstObjGrpReq" value={state.newDestObjGrpReq} onChange={handleChange}>
                   <option value="undefined">-- Please Choose</option>
                   <option value="true">Yes</option>
                   <option value="false">No</option>
                 </select>
-              </label>
-              <label name="ruleEnabled">
+              </label>            
+              )}
+            {(state.newDestIpList !== '' || state.extDestIpList !== '' && 
+              <label name="ruleEnabledL">
                 Would you like the rule enabled upon entry?
-                <select name="ruleEnabled">
+                <select name="ruleEnabled" value={state.ruleEnabled} onChange={handleChange}>
                   <option value="undefined">-- Please Choose</option>
                   <option value="true">Yes</option>
                   <option value="false">No</option>
                 </select>
-              </label>
-              <label name="ruleDescription">
-                Please provide a description of the rule under 28 characters:<input name="ruleDescription" type="text" placeholder="Example: WSUS Inside_Vlan to Outside_Vlan"></input>
-              </label> */}
+              </label>          
+            )}              
+            {(state.ruleEnabled !== '' && 
+              <label name="ruleDescriptionL">
+                Please provide a description of the rule under 28 characters:<input name="ruleDescription" value={state.ruleDescription} onChange={handleChange} type="text" placeholder="Example: WSUS Inside_Vlan to Outside_Vlan"></input>
+              </label>          
+            )}              
+            {(state.ruleDescription !== '' && 
+            <button className="info" style={{ width: '50%', backgroundColor: 'orange', color: 'black',}}>Submit</button>
+            )}
             </form>
           </div>
         </Modal.Body>
@@ -235,7 +280,7 @@ Features To Be Created:
 
 2. Create a slider to push to multiple VDOMs
 
-3. 
+3. onClick submit for final option in form
 
 
 
