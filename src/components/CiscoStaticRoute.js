197906f7e42e9741f7e0568668fb58af6a8c7ddf
Cody Landry <landry.cody@gmail.com>
2022-03-31 22:36:37 -0500

    Working Cisco Static Route Tool.

--- src/components/CiscoStaticRoute.js
+++ src/components/CiscoStaticRoute.js
@@ -1,94 +1,102 @@
-import React, { useState } from "react"
-import { Button, Modal } from "react-bootstrap"
-import { Link } from "react-router-dom"
+import React, {useState} from 'react'
+import {Button, Modal} from 'react-bootstrap'
+import ClipboardCopy from './ClipboardCopy'
+import { Link } from 'react-router-dom'
+import GetMask from './GetMask'
 
-function parseConfig2(fqdnArray, nextHop) {
-  var fqdnList = fqdnArray.split("\n")
-  var nextHopIP = nextHop
-  var ret = fqdnList
-    .map((e) => {
-      return 'ip route ' + e + " " + nextHopIP
+
+function parseConfig2(array, nextHop) {
+    var y = array.split("\n");
+    var z = [];
+    var h = y.map((e) => {
+      var mask2 = GetMask(e);
+      var subnet = e.split("/");
+      var t = "ip route " + subnet[0] + " " + mask2 + " " + nextHop + "\n";
+      return t;
     })
-    .join("\n")
-  return 'config terminal\n' + ret
-}
+    return h.join("");
+    }
 
-function handleCopy(textArea) {
-  navigator.clipboard.writeText(textArea)
-}
+    
+  
 
-function CiscoStaticRoute() {
-  var [showModal, setShow] = useState(true)
-  var handleClose = () => setShow(false)
-  var handleShow = () => setShow(true)
-  var handleShowSettings = () => setShow(true)
-  var [fqdnList, setFqdnList] = useState("")
-  var [output, setOutput] = useState("")
-  var [nextHop, setNextHop] = useState("")
-  var handleChange = (e) => {
-    setFqdnList(e.target.value)
-  }
-  var handleNextHopChange = (e) => {
-    setNextHop(e.target.value)
-    setOutput(parseConfig2(fqdnList, nextHop))
-  }
+function FortigateIPObjects() {
+    var [showModal, setShow] = useState(true)
+    var handleClose = () => setShow(false)
+    var handleShow = () => setShow(true)
+    var [fqdnList, setFqdnList] = useState("")
+    var [output, setOutput] = useState("")  
+    var [nextHop, setNextHop] = useState("")
 
+    var handleChange = (e) => { 
+        if (e.target.value) {
+          setFqdnList(e.target.value);
+          setOutput(parseConfig2(e.target.value, nextHop));
+        } else {
+          setOutput("");
+          setFqdnList("");
+          setNextHop("");
+        }
+      }
 
-  var handleOutput = (e) => {
-    setOutput(parseConfig2(fqdnList, nextHop))
-  }
-
-  return (
-    <div className="appContainer">
-      <div>
-        <h1 className="appTitle">Static Route Generator</h1> <br />
-        <p className="subTitle">This tool will generate the configuration input required to apply a list of IPs to the left text area and the right text area will produce the configuration dynamically.</p>
-        <br />
-        <Button variant="info" className="navLinks" onClick={handleShow}>
-          <li>Static Route Generator</li>
-        </Button>
-      </div>
-
-      <Modal show={showModal} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Static Route Config Generator</Modal.Title>
-          <input name="nextHop" value={nextHop} onChange={handleNextHopChange} type="text" placeholder="Enter your next hop IP"></input>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="row">
-            <div className="col-6">
-              <label>IP Subnet Input (no mask = /32)</label>
-              <textarea className="textFields" value={fqdnList} onChange={handleChange}></textarea>
+      var nextHopChange = (e) => {
+        if (e.target.value != null) {
+          setNextHop(e.target.value);
+        } else {
+          setNextHop("");
+        } 
+      }
+    
+    return (
+        <div className="appContainer">
+        <div>
+          <h1 className="appTitle">Static Route Generator Tool</h1> <br />
+          <p className="subTitle">This tool will generate the configuration input required to create new static routes. Input a list of IP's on the left text area with CIDR and the right will auto-populate with the template. Each IP must be on a line of its own. Please make sure you do not include an extra white space or line in the data.</p>
+          <br />
+          <Button variant="info" className="navLinks" onClick={handleShow}>
+            <li>IP Obj Generator</li>
+          </Button>
+        </div>
+  
+        <Modal show={showModal} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Static Route Generator</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="row">
+              <div className="col-6">
+                <label>IP Input</label>
+                <input placeholder="Input Next Hop IP Here" value={nextHop} onChange={nextHopChange}></input>
+                <textarea className="textFields" value={fqdnList} onChange={handleChange}></textarea>
+              </div>
+              <div className="col-6">
+                <label>Static Route Config</label>
+                <textarea className="textFields" value={output}></textarea>
+              </div>
             </div>
-            <div className="col-6">
-              <label>Configuration</label>
-              <textarea className="textFields" value={output} onChange={handleOutput}></textarea>
-            </div>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="info" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="info" onClick={handleCopy(output)}>
-            Copy to Clipboard
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      <div className="backButton">
-      <Link to="/vendor/cisco/CiscoStaticRoute/settings">
-          <Button variant="info" className="navLinks" onClick={handleShowSettings}>
-            <li>Settings</li>
-          </Button>
-        </Link>
-        <Link to="/vendor/cisco">
-          <Button className="navLinks" variant="info">
-            <li>Back</li>
-          </Button>
-        </Link>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="info" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="info" disabled>
+              Copy to Clipboard
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        <div className="backButton">
+            <Button variant="secondary" className="navLinks" disabled>
+              <li>Settings</li>
+            </Button>
+          <Link to="/vendor/cisco">
+            <Button className="navLinks" variant="info">
+              <li>Back</li>
+            </Button>
+          </Link>
+        </div>
       </div>
-    </div>
-  )
+    )
+
 }
 
-export default CiscoStaticRoute
+export default FortigateIPObjects;
